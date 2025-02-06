616f23d7d68e608e9e6fad78945d5e96fd62c649
danhellem <dahellem@microsoft.com>
2025-02-06 12:59:23 -0500

    Add empty HelloWorld constructor with explanatory comment
    Added an empty constructor `HelloWorld()` to the `HelloWorld` class in the `example_web_app.Classes` namespace. The constructor includes a comment explaining that it is not used in the example but demonstrates how to use a constructor. Also made some formatting changes, such as adding new lines. The `GetMessage` method and the `IHelloWorld` interface remain unchanged.
    
    AB#114382

--- example-web-app/Classes/HelloWorld.cs
+++ example-web-app/Classes/HelloWorld.cs
@@ -2,10 +2,18 @@
 {
     public class HelloWorld : IHelloWorld
     {
+        // This method is not used in the example, but it is here to show how to use a constructor  
+        public HelloWorld()
+        {
+
+        }
+
         public string GetMessage()
         {
             return "Hello, World!";
         }
+
+       
     }
 
     public interface IHelloWorld
